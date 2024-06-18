79403f0c69825dc6fd06fdf813bfc2c29a9a9bf4
Uday <udayshankar666@gmail.com>
2024-06-18 20:27:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const errorController = require("./controllers/error");
 const User = require("./models/user");
 
 const MONGODB_URI =
-  "mongodb+srv://udayshankar:TwyVi9cc2N0MMttc@cluster0.jltht.mongodb.net/shop";
+  "mongodb+srv://*****@cluster0.jltht.mongodb.net/shop";
 
 const app = express();
 const store = new MongoDBStore({
